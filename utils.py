7ddb1e2cece890830ce332c28b90a8561db34fa6
Daniel Robayo <87443087+danielrobapaz@users.noreply.github.com>
2023-07-10 00:16:52 -0400

    salamaleco

--- utils.py
+++ utils.py
@@ -35,7 +35,7 @@ def get_rest_1(n_players, n_days, n_hours, possible_games):
                 x = possible_games.index(set[0])+1
                 y = possible_games.index(set[1])+1
 
-                rest.append(f"-{x} -{y} 0")
+                rest.append(f"-{x} -{y} 0\n")
     return rest
 
 # 2. Todos los participantes deben jugar dos veces con cada uno de los otros participantes, una
@@ -65,53 +65,81 @@ def get_rest_2(n_players, n_days, n_hours, possible_games):
 # 3. Un jugador solo puede jugar maximo una vez por dia
 # CNF: not a or not b or not c
 # not X(j1, j2, d, h1) or not X(j1, j3, d, h2) or not X(j3, j1, d, h2)
-
 def get_rest_3(n_players, n_days, n_hours, possible_games):
-    rest_3 = []
+    rest = []
+
     for j1 in range(n_players):
-        for j2 in range(n_players):
-            for d in range(n_days):
-                for h1 in range(n_hours):
-                        for j3 in range(n_players):
-                            for h2 in range(n_hours):
-                                if h1 != h2 and j1 != j2 and j1 != j3 and j2 != j3:
-                                    print("f")
-    return rest_3
+        for d in range(n_days):
+            curr_posible_games = []
+            for j2 in range(n_players):
+                for h in range(n_hours):
+                    if j1 != j2:
+                        curr_posible_games.append((j1, j2, d, h))
+                        curr_posible_games.append((j2, j1, d, h))
+    
+            subsets = combinations(curr_posible_games, 2)
+            for set in subsets:
+                x = possible_games.index(set[0])+1
+                y = possible_games.index(set[1])+1
+
+                rest.append(f"-{x} -{y} 0\n")
+
+    return rest
 
 # 4. Un participante no puede jugar de "visitante" en dos días consecutivos,
 # ni de "local" dos días seguidos.
 # CNF: not a or not b
 # not X(j1, j2, d1, h1) or not X(j1, j2, d2, h2)
 def get_rest_4(n_players, n_days, n_hours, possible_games):
-    rest_4 = []
+
+    rest = []
+    # un equipo no puede jugar local dos dias consecutivos seguidos
     for j1 in range(n_players):
-        for j2 in range(n_players):
-            for j3 in range(n_players):
-                for d in range(n_days-1):
-                    for h1 in range(n_hours):
-                        for h2 in range(n_hours):
-                            if j1 != j2 and j1 != j3:
-                                x = possible_games.index((j1, j2, d, h1)) + 1
-                                y = possible_games.index((j1, j3, d+1, h2)) + 1
-                                
-                                rest_4.append(f"-{x} - {y} 0")
+        for d in range(n_days-1):
+            curr_posible_games = []
+            for j2 in range(n_players):
+                for h in range(n_hours):
+                    if j1 != j2:
+                        curr_posible_games.append((j1, j2, d, h))
+                        curr_posible_games.append((j1, j2, d+1, h))
 
-                                x = possible_games.index((j2, j1, d, h1)) + 1
-                                y = possible_games.index((j3, j1, d+1, h2)) + 1
+            subsets = combinations(curr_posible_games, 2)
+            for set in subsets:
+                x = possible_games.index(set[0])+1
+                y = possible_games.index(set[1])+1
 
-                                rest_4.append(f"-{x} - {y} 0")
-    return rest_4
+                rest.append(f"-{x} -{y} 0\n")
+
+
+    # un equipo no puede jugar dos dias consecutivos como visitante
+    for j1 in range(n_players):
+        for d in range(n_days-1):
+            curr_posible_games = []
+            for j2 in range(n_players):
+                for h in range(n_hours):
+                    if j1 != j2:
+                        curr_posible_games.append((j2, j1, d, h))
+                        curr_posible_games.append((j2, j1, d, h))
+
+            subsets = combinations(curr_posible_games, 2)
+            for set in subsets:
+                x = possible_games.index(set[0])+1
+                y = possible_games.index(set[1])+1
+
+                rest.append(f"-{x} -{y} 0\n")
+    
+    return rest
 
 def create_dimacs_file(x, rest_1, rest_2, rest_3, rest_4):
     # create the dimacs file
     f = open("tournament.dimacs", "w")
-    f.write(f"p cnf {len(x)} {len(rest_1)} {len(rest_2)} {len(rest_3)} {len(rest_4)}\n")
+    f.write(f"p cnf {len(x)} {len(rest_1)+len(rest_3)+len(rest_4)}\n")
     for rest in rest_1:
-        f.write(f"{rest} \n")
-    for rest in rest_2:
-        f.write(f"{rest} \n")
+        f.write(f"{rest}")
+    #for rest in rest_2:
+    #    f.write(f"{rest} \n")
     for rest in rest_3:
-        f.write(f"{rest} \n")
+        f.write(f"{rest}")
     for rest in rest_4:
-        f.write(f"{rest} \n")
+        f.write(f"{rest}")
     f.close()
